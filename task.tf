b7db8f83c94aa658429e0e9aabb8c080d2f3a12b
David Třebický <davidtrebicky@gmail.com>
2020-07-01 16:06:54 +0200

    fix task definition

--- task.tf
+++ task.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_task_definition" "application" {
   network_mode             = "bridge"
   requires_compatibilities = ["EC2"]
 
-  container_definitions = ["${module.container_definition.json_map}, ${module.container_definition_nginx.json_map}"]
+  container_definitions = "[${module.container_definition.json_map},${module.container_definition_nginx.json_map}]"
 
   volume {
     name = "nginx_config"
